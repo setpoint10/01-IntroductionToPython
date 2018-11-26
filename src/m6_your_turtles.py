91884cd470cd7492c121713b9c08e75b8cf8d315
xiny <xiny@rose-hulman.edu>
2018-11-26 11:19:33 -0500

    done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Yu Xin.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,29 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+
+import rosegraphics as rg
+window = rg.TurtleWindow()
+first = rg.SimpleTurtle("turtle")
+second = rg.SimpleTurtle("square")
+size = 360
+
+window.tracer(5)
+
+for k in range(10):
+    first.forward(100 - k)
+    first.left(size/4 - 2 )
+    first.backward(k)
+    first.draw_circle(5*k)
+    size=size-2
+
+for k in range(10):
+    second.draw_square(10*k)
+    second.forward(10)
+    second.pen_up()
+    second.right(90)
+    second.pen_down()
+    second.backward(k)
+    second.left(90)
+
+window.close_on_mouse_click()
